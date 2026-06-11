5231871de393f81f11ba30b1da45e4e34ae00e1e
Dixit106 <dixitk2958@gmail.com>
2026-06-11 20:14:14 +0530

    importing argparase python module and making a function for taking username of the target

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
-print("Let's get started!")
+import argparse
+
+def main():
+    #setting up argument parser
+    parser = argparse.ArgumentParser(description="A Simple OSINT Recon Tool")
+
+    #argument for target username
+    parser.add_argument("-u", "--username", help="The username to investigate")
+
+    #parse the inputs
+    args = parser.parse_args()
+
+    #check if user actually provide a username
+    if args.username:
+        print(f"[*] Target Username Locked: {args.username}")
+    else:
+        print("[!] Error: Please provide a username using -u or --username")
+
+if __name__ == "__main__":
+    main()        
