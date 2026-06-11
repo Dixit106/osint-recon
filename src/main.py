df08619e6f1a54f997d867489694ed9c669a6f9a
Dixit106 <dixitk2958@gmail.com>
2026-06-11 20:45:08 +0530

    finally now using request module i can find multiple people on github by username

--- src/main.py
+++ src/main.py
@@ -1,4 +1,27 @@
 import argparse
+import requests
+
+def check_username(username):
+    print(f"\n[*] Scanning for username: {username}...")
+
+    #Target URL(Github)
+    url = f"https://github.com/{username}"
+
+    try:
+        #HTTP GET request to the profile
+        response = requests.get(url)
+
+        # 200 OK would mean the page exists
+        if response.status_code == 200:
+            print(f"[+] FOUND: {url}")
+        #404 Not Found means the username available/doesn't exist
+        elif response.status_code == 404:
+            print(f"[-] NOT FOUND: GitHub profile does not exist.")
+        else:
+            print(f"[?] UNKNOWN: Received status code {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"[!] Connection Error: {e}")            
 
 def main():
     #setting up argument parser
@@ -13,6 +36,7 @@ def main():
     #check if user actually provide a username
     if args.username:
         print(f"[*] Target Username Locked: {args.username}")
+        check_username(args.username)
     else:
         print("[!] Error: Please provide a username using -u or --username")
 
