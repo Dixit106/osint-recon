4f9171d2bc6b8ef1279d598ce1c28a3f46c57937
Dixit106 <dixitk2958@gmail.com>
2026-06-12 14:44:47 +0530

    adding api to find about domain ip addr and location etc

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import argparse
 import requests
+import socket
 
 def check_username(username):
     print(f"\n[*] Scanning across multiple platforms for: {username}...\n")
@@ -29,14 +30,42 @@ def check_username(username):
                 print(f"[?] UNKNOWN: Received status code {response.status_code}")
 
         except requests.exceptions.RequestException as e:
-            print(f"[!] Connection Error: {e}")            
+            print(f"[!] Connection Error: {e}")
 
+def check_domain(domain):
+    print(f"\n[*] Investigating Domain: {domain}...\n")
+    try:
+        #Resolve domain to an IP address
+        ip_address = socket.gethostbyname(domain)
+        print(f"[+] Target IP Address: {ip_address}")
+
+        #free api to locate the IP address
+        geo_url = f"https://ipapi.co/{ip_address}/json/"
+        headers = {"User-Agent": "Mozilla/5.0"}
+        response= requests.get(geo_url, headers=headers, timeout=5)
+
+        if response.status_code == 200:
+            data = response.json()
+            print(f"[+] ISP/Hosting: {data.get('org', 'Unknown')}")
+            print(f"[+] Location: {data.get('city', 'Unknown')}, {data.get('region', "Unknown")}, {data.get('country_name','Unknown')}")                        
+
+        else:
+            print("[-] Could not retrieve geolocation details.")
+
+    except socket.gaierror:
+        print("[!] Error: Invalid domain name or connection failure.")
+    except Exception as e:
+        print(f"[!] Error gathering intel: {e}")
+                        
 def main():
     #setting up argument parser
     parser = argparse.ArgumentParser(description="A Simple OSINT Recon Tool")
 
     #argument for target username
     parser.add_argument("-u", "--username", help="The username to investigate")
+
+    #argument for targetting domain
+    parser.add_argument("-d", "--domain", help="The domain name to investigate")
 
     #parse the inputs
     args = parser.parse_args()
@@ -45,8 +74,11 @@ def main():
     if args.username:
         print(f"[*] Target Username Locked: {args.username}")
         check_username(args.username)
+    elif args.domain:
+        print(f"[*] Target Domain Locked: {args.domain}")
+        check_domain(args.domain)    
     else:
-        print("[!] Error: Please provide a username using -u or --username")
+        print("[!] Error: Please provide an argument. Use -h or --help for instructions.")
 
 if __name__ == "__main__":
     main()        
