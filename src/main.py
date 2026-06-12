938784000d127f4648e43d6dd990c4fc15293a3b
Dixit106 <dixitk2958@gmail.com>
2026-06-12 14:16:59 +0530

    adding other websites too to search for given user name

--- src/main.py
+++ src/main.py
@@ -2,26 +2,34 @@ import argparse
 import requests
 
 def check_username(username):
-    print(f"\n[*] Scanning for username: {username}...")
+    print(f"\n[*] Scanning across multiple platforms for: {username}...\n")
 
-    #Target URL(Github)
-    url = f"https://github.com/{username}"
+    #Creating a dictionary of platforms and their URL structures
+    platforms = {
+        "GitHub": f"https://github.com/{username}",
+        "Snapchat": f"https://story.snapchat.com/@{username}",
+        "Linktree": f"https://linktr.ee/{username}"
+    }
 
-    try:
+#looping through each platform
+    for platforms, url in platforms.items():
+        try:
+        # We need user-Agent so website don't immediately block out bot
+            headers = {"User-Agent": "Mozilla/5.0"}
         #HTTP GET request to the profile
-        response = requests.get(url)
+            response = requests.get(url, headers=headers, timeout=5)
 
         # 200 OK would mean the page exists
-        if response.status_code == 200:
-            print(f"[+] FOUND: {url}")
+            if response.status_code == 200:
+                print(f"[+] FOUND on {platforms}: {url}")
         #404 Not Found means the username available/doesn't exist
-        elif response.status_code == 404:
-            print(f"[-] NOT FOUND: GitHub profile does not exist.")
-        else:
-            print(f"[?] UNKNOWN: Received status code {response.status_code}")
+            elif response.status_code == 404:
+                print(f"[-] NOT FOUND on {platforms}")
+            else:
+                print(f"[?] UNKNOWN: Received status code {response.status_code}")
 
-    except requests.exceptions.RequestException as e:
-        print(f"[!] Connection Error: {e}")            
+        except requests.exceptions.RequestException as e:
+            print(f"[!] Connection Error: {e}")            
 
 def main():
     #setting up argument parser
